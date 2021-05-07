c54f2fea6f668ea51c22d001289ed5a3c099e1f2
scotmachina <scott@machinalabs.ai>
2021-05-06 17:58:34 -0700

    handle event covers all three cases, and requisite tests added

--- chapter_02/line_segment_intersection.py
+++ chapter_02/line_segment_intersection.py
@@ -3,18 +3,33 @@ import numpy as np
 from dataclasses import dataclass
 from enum import IntEnum
 from matplotlib import pyplot as plt
-from matplotlib import collections  as mc
+from matplotlib import collections as mc
 
 
-def gen_rand_lines(num_lines=10, grid_points: Optional[int] = None):
-    # produces a #N, 2, 2 numpy array corresponding to a lot of lines
+def gen_rand_lines(num_lines=10, grid_points: Optional[int] = None) -> Tuple[List, List]:
+    """
+
+    Args:
+        num_lines: Number of lines to make
+        grid_points: number of discrete x and y values to use
+
+    Returns:
+        A tuple comprising:
+            list of points, each is a list of two floats
+            list of lines, each is a list of indices into the points list
+
+    """
     points = np.random.rand(num_lines * 2, 2)
     if grid_points:
         points = (points * grid_points).astype(int) / grid_points
-    return points, np.arange(0, num_lines * 2).reshape(num_lines, 2)
+    res_points = [list(p) for p in points]
+    res_lines = [list(l) for l in np.arange(0, num_lines * 2).reshape(num_lines, 2)]
+    return res_points, res_lines
 
 
-def plot_lines(points: List[np.ndarray], lines: np.ndarray, show=True):
+def plot_lines(points: List, lines: List, show=True):
+    points = np.asarray(points)
+    lines = np.asarray(lines)
     # expects numpy array of shape #N, 2, 2
     mp_lines = [[(points[l[0]][0], points[l[0]][1]), (points[l[1]][0], points[l[1]][1])] for l in lines]
 
@@ -34,6 +49,9 @@ def plot_lines(points: List[np.ndarray], lines: np.ndarray, show=True):
 
 
 def line_sort(points, lines):
+    points = np.asarray(points)
+    lines = np.asarray(lines)
+
     lines_flipped = np.fliplr(lines)
     p1_y = points[lines[:, 0]][:, 1]
     p2_y = points[lines[:, 1]][:, 1]
@@ -44,8 +62,9 @@ def line_sort(points, lines):
         np.lexsort((points[lines_oriented[:, 0]][:, 0], points[lines_oriented[:, 0]][:, 1]), axis=0)]
     lines_sorted = np.flipud(lines_sorted)
 
-    return points, lines_sorted
-
+    points = [list(p) for p in points]
+    lines = [list(l) for l in lines_sorted]
+    return points, lines
 
 
 def point_comp(p1, p2):
@@ -71,7 +90,7 @@ class Event:
     parents: List[int]
 
 
-def calc_segment_intersection(a1:  Union[List[int], np.ndarray],
+def calc_segment_intersection(a1: Union[List[int], np.ndarray],
                               a2: Union[List[int], np.ndarray],
                               b1: Union[List[int], np.ndarray],
                               b2: Union[List[int], np.ndarray]) -> Optional[np.ndarray]:
@@ -82,54 +101,104 @@ def calc_segment_intersection(a1:  Union[List[int], np.ndarray],
     a2 = np.asarray(a2)
     b1 = np.asarray(b1)
     b2 = np.asarray(b2)
-    da = a2-a1
-    db = b2-b1
-    dp = a1-b1
-    dap = [-da[1], da[0]] # maybe convert to numpy
+    da = a2 - a1
+    db = b2 - b1
+    dp = a1 - b1
+    dap = [-da[1], da[0]]  # maybe convert to numpy
     u = (np.dot(dap, dp) / np.dot(dap, db))
     ip = u * db + b1
-    v = np.dot(da, ip - a1)/np.linalg.norm(da)**2
+    v = np.dot(da, ip - a1) / np.linalg.norm(da) ** 2
     if u < 0 or u > 1 or v < 0 or v > 1:
         return None
     return ip
 
 
-def check_status(status: List[int],
-                 index_a: int,
-                 index_b: int,
-                 points: np.ndarray,
-                 lines: np.ndarray) -> Tuple[Optional[np.ndarray], Optional[Event]]:
+def modify_status(status: List[int],
+                  event_queue: List[Event],
+                  index_a: int,
+                  index_b: int,
+                  points: List,
+                  lines: List):
+    """
+    Determines if there is an intersection between status[index_a] and status[index_b]
+    If there is, it inserts (in place) an event into event_queue with event_type INTER
+    """
+
     l1 = lines[status[index_a]]
     l2 = lines[status[index_b]]
-    l1p1, l1p2 = points[l1]
-    l2p1, l2p2 = points[l2]
-    l_int = calc_segment_intersection(l1p1, l1p2, l2p1, l2p2)
-    # if l_int:
-    #     return Event(len(points), )
+    l1p1, l1p2 = (points[l1][0], points[l1][1])
+    l2p1, l2p2 = (points[l2][0], points[l2][1])
+    int_p = calc_segment_intersection(l1p1, l1p2, l2p1, l2p2)
+    if int_p:
+        points.append(int_p)
+        inter_event = Event(len(points) - 1, EventType.INTER, [status[index_a], status[index_b]])
+        for i, e in enumerate(event_queue):
+            if point_comp(points[e.point_idx], int_p):
+                event_queue.insert(i - 1, inter_event)
+                break
 
-def handle_event(event: Event, event_queue: List[Event], status: List[int], points: np.ndarray, lines: np.ndarray):
-    # TODO: Turn point array into a list of points so we can append in place inside check_status
-    # TODO: Bonus points if you make lines a list as well for consistency
 
+def handle_event(event: Event, event_queue: List[Event], status: List[int], points: List, lines: List):
     if event.e_type == EventType.STARTPT:
-        # add endpoint to event queue, insert that into status, check status
+        # add endpoint to event queue, insert that into status, modify status
+        start_point_idx = lines[event.parents[0]][0]
         end_point_idx = lines[event.parents[0]][1]
-
+        start_point = points[start_point_idx]
         end_point = points[end_point_idx]
         new_event = Event(end_point_idx, EventType.ENDPT, [event.parents[0]])
+
+        stat_idx = len(status)
+        for i, _ in enumerate(status):
+            if point_comp(points[lines[status[i]][0]], start_point):
+                stat_idx = i - 1
+                break
+        status.insert(stat_idx, event.parents[0])
+
+        event_idx = len(event_queue)
         for i, e in enumerate(event_queue):
             if point_comp(points[e.point_idx], end_point):
-                event_queue.insert(i-1, new_event)
-                return
-        event_queue.insert(len(event_queue), new_event)
+                event_idx = i - 1
+                break
+        event_queue.insert(event_idx, new_event)
+
+        if stat_idx != 0:
+            modify_status(status, event_queue, stat_idx - 1, stat_idx, points, lines)
+        if stat_idx <= len(status) - 2:
+            modify_status(status, event_queue, stat_idx, stat_idx + 1, points, lines)
+
     if event.e_type == EventType.ENDPT:
         # remove line from status, check status
-        pass
+        l_idx_rem = event.parents[0]
+        # The line had better be in the status array, or we've already screwed something up.
+        status_l_idx = status.index(l_idx_rem)
+        del status[l_idx_rem]
+        # Only check status if the line was not on an end of the status array
+        if status_l_idx != 0 and status_l_idx != len(lines):
+            modify_status(status, event_queue, status_l_idx - 1, status_l_idx, points, lines)
     if event.e_type == EventType.INTER:
         # swap some stuff, check status
-        pass
+        parents = event.parents
+
+        par_1_stat_idx = status.index(parents[0])
+        par_2_stat_idx = status.index(parents[1])
+
+        assert abs(par_1_stat_idx - par_2_stat_idx) < 2, "Parents are not adjacent in status. Investigate."
+        par_left_stat_idx, par_right_stat_idx = sorted([par_1_stat_idx, par_2_stat_idx])
+
+        temp_event = status[par_left_stat_idx]
+        status[par_left_stat_idx] = status[par_right_stat_idx]
+        status[par_right_stat_idx] = temp_event
+
+        # Left status element check
+        if par_left_stat_idx != 0:
+            modify_status(status, event_queue, par_left_stat_idx - 1, par_left_stat_idx, points, lines)
+        if par_right_stat_idx <= len(status) - 2:
+            modify_status(status, event_queue, par_right_stat_idx, par_right_stat_idx + 1, points, lines)
+
 
 def calc_intersections(points: np.ndarray, lines: np.ndarray):
+    #TODO: Run through the algorithm and make sure that this makes sense.
+    #"this is gonna work on the first try" -Scott
     _, lines = line_sort(points, lines)
     events = [(l, p, False) for l, p in enumerate(lines[:, 0])]
     events.reverse()
@@ -145,16 +214,13 @@ def calc_intersections(points: np.ndarray, lines: np.ndarray):
         # If the event point corresponds to a line we've added the first point
         # of, add the second point of the line to our events
         if not has_seen_line:
-
             line = lines[line_idx]
             print(points[line[1]])
             ins_idx = 0
             print(point, events)
             # TODO (Max and Scott): Insert the bottom point of the line into
             # our event list. 
-            for event in events:
-                if point_comp(points[line[1]], points[event[1]]):
-                    ins_idx = event[1]
+
             print(ins_idx)
         # If it's a second point in a line, we remove the line from status or
         # something.
@@ -178,5 +244,3 @@ if __name__ == "__main__":
     # calc_intersections(*gen_rand_lines(3))
 
     pass
-
-
--- chapter_02/test_line_segment_intersection.py
+++ chapter_02/test_line_segment_intersection.py
@@ -3,23 +3,32 @@ from line_segment_intersection import *
 
 
 class TestLSI(unittest.TestCase):
+    def test_gen_rand_lines(self):
+        points, lines = gen_rand_lines(3, 10)
+        self.assertTrue(all(isinstance(p, list) for p in points))
+        self.assertTrue(all(isinstance(l, list) for l in lines))
+        self.assertTrue(len(points) == 6)
+        self.assertTrue(len(lines) == 3)
+
     def test_line_sort(self):
-        points = np.asarray([
+        points = [
             [2, 9],
             [10, 3],
             [2, 2],
             [1, 0],
             [8, 5],
             [4, 1]
-        ])
+        ]
 
-        lines = np.asarray([
+        lines = [
             [0, 1],
             [2, 3],
             [4, 5]
-        ])
+        ]
 
         points, lines = line_sort(points, lines)
+        points = np.asarray(points)
+        lines = np.asarray(lines)
         self.assertTrue(np.all(np.diff(points[lines[:, 1]][:, 1]) < 0))
 
     def test_calculate_segment_intersection(self):
@@ -39,31 +48,80 @@ class TestLSI(unittest.TestCase):
         self.assertEqual(res, None)
 
     def test_handle_event_startpt(self):
-        points = np.asarray([
+        points = [
             [2, 9],
             [10, 3],
             [2, 6],
             [1, 2],
             [8, 5],
             [4, 1]
-        ])
+        ]
 
-        lines = np.asarray([
+        lines = [
             [0, 1],
             [2, 3],
             [4, 5]
-        ])
+        ]
 
         event_queue = [Event(lines[i][0], EventType.STARTPT, [i]) for i in range(3)]
         points, lines = line_sort(points, lines)
 
-        points[2, 1] = 2
-        points[3, 1] = 0
+        points[2][1] = 2
+        points[3][1] = 0
         points, lines = line_sort(points, lines)
         event = event_queue.pop(0)
-
-        handle_event(event, event_queue, [], points, lines)
+        status = []
+        handle_event(event, event_queue, status, points, lines)
+        # TODO: test that adding starpoints modifies the status appropriately
         self.assertTrue(event_queue[1].e_type == EventType.ENDPT)
+
+    def test_handle_event_endpt(self):
+        points = [
+            [2, 9],
+            [10, 8],
+            [2, 6],
+            [1, 2],
+            [8, 5],
+            [4, 1]
+        ]
+
+        lines = [
+            [0, 1],
+            [2, 3],
+            [4, 5]
+        ]
+        status = [0]
+        e = Event(1, EventType.ENDPT, [0])
+        handle_event(e, [], status, points, lines)
+        self.assertTrue(len(status) == 0)
+
+    def test_handle_event_interpt(self):
+        points = [
+            [2, 9],
+            [10, 3],
+            [2, 6],
+            [1, 2],
+            [8, 5],
+            [4, 1],
+            [7.71, 4.71]
+        ]
+
+        lines = [
+            [0, 1],
+            [2, 3],
+            [4, 5]
+        ]
+
+        # plot_lines(points, lines)
+        status = [1, 0, 2]
+        e = Event(6, EventType.INTER, [0, 2])
+        handle_event(e, [], status, points, lines)
+        self.assertTrue(status == [1, 2, 0])
+
+        status = [1, 0, 2]
+        e = Event(6, EventType.INTER, [2, 0])
+        handle_event(e, [], status, points, lines)
+        self.assertTrue(status == [1, 2, 0])
 
     def test_point_comp(self):
         sim_event_points = np.asarray([
@@ -74,6 +132,47 @@ class TestLSI(unittest.TestCase):
         res = [False, False, True]
         for p, r in zip(sim_event_points, res):
             self.assertEqual(point_comp(p, [10, 3]), r)
+
+    def test_modify_status(self):
+        points = [
+            [3, 9],
+            [10, 3],
+            [2, 6],
+            [1, 2],
+            [9, 5],
+            [4, 1],
+        ]
+
+        lines = [
+            [0, 1],
+            [2, 3],
+            [4, 5]
+        ]
+        pass
+        # plot_lines(points, lines)
+        # status = [0, 1]
+        #
+        # e = Event(6, EventType.INTER, [0, 2])
+        # modify_status(status, [], )
+
+    def test_intersections(self):
+        points = [
+            [3, 9],
+            [10, 3],
+            [2, 6],
+            [1, 2],
+            [9, 5],
+            [4, 1],
+        ]
+
+        lines = [
+            [0, 1],
+            [2, 3],
+            [4, 5]
+        ]
+        calc_intersections(points, lines)
+
+
 
 
 if __name__ == "__main__":
